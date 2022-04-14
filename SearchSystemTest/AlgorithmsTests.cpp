06fdb86b29a963ff7845f1961ae2901b1dabdf72
Yaroslav <suckbro@yandex.ru>
2022-04-14 19:15:56 +0300

    Add test project

---
+++ SearchSystemTest/AlgorithmsTests.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+#include "../SearchSystem/Include/Algorithms.h"
+
+using namespace testing;
+
+TEST(AlgorithmsTests, SplitIntoWordsTest)
+{
+    EXPECT_EQ((std::vector<std::string>{"asd", "dsa", "zxc"})
+              , Algorithms::Algorithms::SplitIntoWords("asd dsa zxc"));
+}
+
+TEST(AlgorithmsTests, CountStartsWithTest)
+{
+    EXPECT_EQ(1, Algorithms::Algorithms::CountStartsWith(std::vector<std::string>{"asd", "dsa", "zxc"}, 'a'));
+    EXPECT_EQ(3, Algorithms::Algorithms::CountStartsWith(std::vector<std::string>{"dasd", "dsa", "d"}, 'd'));
+    EXPECT_EQ(0, Algorithms::Algorithms::CountStartsWith(std::vector<std::string>{"asd", "dsa", "zxc"}, 'c'));
+}
+
+TEST(AlgorithmsTests, CCalculateMedianTest)
+{
+    EXPECT_EQ(std::make_pair(true, 2.0)
+              , Algorithms::Algorithms::CalculateMedian(std::vector<double>{1, 2, 3}));
+    EXPECT_EQ(std::make_pair(true, 2.5)
+              , Algorithms::Algorithms::CalculateMedian(std::vector<double>{1, 2, 3, 4}));
+    EXPECT_EQ(std::make_pair(false, 0.0)
+              , Algorithms::Algorithms::CalculateMedian(std::vector<double>{}));
+}
---
+++ SearchSystemTest/SearchSystemTest.pro
@@ -0,0 +1,16 @@
+include(gtest_dependency.pri)
+
+TEMPLATE = app
+CONFIG += console c++20
+CONFIG -= app_bundle
+CONFIG += thread
+CONFIG -= qt
+INCLUDEPATH += "../SearchSystem/Include"
+
+SOURCES += \
+        ../SearchSystem/Source/Algorithms.cpp \
+        AlgorithmsTests.cpp \
+        main.cpp        
+
+HEADERS += \
+    ../SearchSystem/Include/Algorithms.h
